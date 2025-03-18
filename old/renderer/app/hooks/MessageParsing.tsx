47f0c6798cc3ffdf3876ae5538f4d493b21abb21
TheGuyWithoutH <ugo.balducci@hotmail.fr>
2025-03-18 02:01:35 +0100

    fix: remove old

--- old/renderer/app/hooks/MessageParsing.tsx
+++
@@ -1,8 +0,0 @@
-import { marked } from "marked";
-import DOMPurify from "dompurify";
-
-const responseParser = (response: string) => {
-  return DOMPurify.sanitize(marked.parse(response.split("</s>")[0]));
-};
-
-export default responseParser;
